4c4019a289c0c518dca00cf7c34e9ace54b188d3
Ravi kumar <rkravikr9102@gmail.com>
2022-08-08 15:09:30 +0530

    Done Assignment

---
+++ block-BNaaev/sample/models/user.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+
+var userSchema = new Schema({
+    name: { type: String, required: true},
+    email: { type: String, required: true, unique: true},
+    password: { type: String, minlength: 5},
+    age: Number,
+    phone: Number
+}, { timestamps: true});
+
+userSchema.pre('save', async function(next){
+    if(this.password && this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password,10)
+        return next();
+    } else{
+        next();
+    }
+});
+
+module.exports = mongoose.model('User',userSchema);
